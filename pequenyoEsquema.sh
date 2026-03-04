4624c1cf472ac86b153028b532bff448132a136a
Nestor <nestorblancotech@gmail.com>
2026-03-04 12:04:29 +0100

    Progreso en el pequenyo diseño
    Co-authored-by: Hector Alejaldre Martin <heeectoor21@users.noreply.github.com>

--- pequenyoEsquema.sh
+++ pequenyoEsquema.sh
@@ -2,13 +2,40 @@
 
 # Parámetros: $1: {-a|-s}, $2: <nombre_fichero>
 
+# Comprobación de permisos
+#if [[ "$EUID" -ne 0 ]]
+#then
+#    echo "Este script necesita privilegios de administracion" 
+#    exit 1
+#fi
+
+# Comprobación de parámetros
 if [[ "$#" -ne 2 ]]
 then
     echo "Número incorrecto de parámetros"
     exit 1
 fi
 
-OPCION = $1
-FICHERO = $2
+OPCION=$1
+FICHERO=$2
+
+# Comprobación de opción
+if [[ ! "$OPCION" =~ ^(-a|-s)$ ]]
+then
+    echo "Opción inválida" <&2
+    exit 1
+fi
+
+# Comprobar archivo de log
+FECHA=$(date +"%Y_%m_%d")
+USUARIO=$(whoami)
+
+dir_destino=$(ls "$FECHA"_"$USUARIO"_provisioning.log 2>/dev/null)
+
+if [[ -z "$dir_destino" ]]
+then
+    touch "$FECHA"_"$USUARIO"_provisioning.log
+    dir_destino=$(ls "$FECHA"_"$USUARIO"_provisioning.log 2>/dev/null)
+fi
 
 
